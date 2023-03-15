cc2227b50763e7206ca0774de3aa86798b9794cb
Egor Zhuravlev <zhuravlevegor@gmail.com>
2023-03-15 19:34:52 +0300

    Find added

---
+++ find.py
@@ -0,0 +1,14 @@
+def find_number(contact_list):
+    from search import search_parameters
+    from print_file import print_contacts
+    search_field, search_value = search_parameters()
+    search_value_dict = {'1': 'Фамилия', '2': 'Имя', '3': 'Номер телефона'}
+    found_contacts = []
+    for contact in contact_list:
+        if contact[search_value_dict[search_field]] == search_value:
+            found_contacts.append(contact)
+    if len(found_contacts) == 0:
+        print('Контакт не найден!')
+    else:
+        print_contacts(found_contacts)
+    print()
