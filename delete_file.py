8f2a09fbcdb72324f8087252ac4405caca7d3cd7
Egor Zhuravlev <zhuravlevegor@gmail.com>
2023-03-15 19:32:58 +0300

    Delete_file added

---
+++ delete_file.py
@@ -0,0 +1,10 @@
+def delete_contact(file_name):
+    from read_list import read_file_to_list
+    from modify_search import search_to_modify
+    contact_list = read_file_to_list(file_name)
+    number_to_change = search_to_modify(contact_list)
+    contact_list.remove(number_to_change)
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for contact in contact_list:
+            line = ' '.join(contact) + '\n'
+            file.write(line)
