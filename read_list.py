553e3b6e0667f0794c89486e9bb67c604563bac5
Egor Zhuravlev <zhuravlevegor@gmail.com>
2023-03-16 20:49:59 +0300

    Read_list modified

--- read_list.py
+++ read_list.py
@@ -1,5 +1,5 @@
-def read_file_to_list(file_name):
-    with open(file_name, 'r', encoding='utf-8') as file:
+def read_file_to_list(phonebook):
+    with open(phonebook, 'r', encoding='utf-8') as file:
         contact_list = []
         for line in file.readlines():
             contact_list.append(line.split())
