3a12cab8bc071ccfdbe7a27f162d2796ad02df18
Egor Zhuravlev <zhuravlevegor@gmail.com>
2023-03-16 20:48:33 +0300

    Delete_file modified

--- delete_file.py
+++ delete_file.py
@@ -1,10 +1,10 @@
-def delete_contact(file_name):
+def delete_contact(phonebook):
     from read_list import read_file_to_list
     from modify_search import search_to_modify
-    contact_list = read_file_to_list(file_name)
+    contact_list = read_file_to_list(phonebook)
     number_to_change = search_to_modify(contact_list)
     contact_list.remove(number_to_change)
-    with open(file_name, 'w', encoding='utf-8') as file:
+    with open(phonebook, 'w', encoding='utf-8') as file:
         for contact in contact_list:
             line = ' '.join(contact) + '\n'
             file.write(line)
